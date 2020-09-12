67621b42e56a4a234edc2dcfb0460e8e9c657195
Ilya Fadeev <fadyaIlya@yandex.ru>
2020-09-13 00:46:58 +0300

    start developing

---
+++ src/NNet.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nncli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/NNet/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NNet
+{
+    public class Class1
+    {
+    }
+}
