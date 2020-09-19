4d93f1da6f778e172aec9d70585af3fb34ce41aa
Ilya Fadeev <fadyaIlya@yandex.ru>
2020-09-19 22:30:16 +0300

    delete an useless folder

---
+++ src/NNet.Test/Network/BuilderTests.cs
@@ -0,0 +1,120 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NNet;
+using NNet.IO;
+using NNet.Builder;
+using NNet.Neurons;
+using NNet.Functions;
+
+namespace NNet.Test.Network
+{
+    [TestClass]
+    public class BuilderTests
+    {
+        [TestMethod]
+        public void SetInputSize_InputIs5_ResultsIntputSizeIs5()
+        {
+            var result = NeuralNetwork.Builder.SetInputSize(5)
+                                              .Build()
+                                              .ToNetwork();
+
+            Assert.AreEqual(5, result.InputSize);
+        }
+
+        [TestMethod]
+        public void AddLayer_InputIsLayersInformation_ResultsLayersCountIs3()
+        {
+            var result = NeuralNetwork.Builder.AddLayer(1, ActivationFunctionType.Linear)
+                                              .AddLayer(2, ActivationFunctionType.Linear)
+                                              .AddLayer(3, ActivationFunctionType.Linear)
+                                              .Build()
+                                              .ToNetwork();
+
+            Assert.AreEqual(3, result.LayersCount);
+        }
+
+        [TestMethod]
+        public void AddLayer_InputIsSizesAndFunctions_ResultsLayersCountIs3()
+        {
+            var result = NeuralNetwork.Builder.AddNeurons(1, 2, 3)
+                                              .AddFunctions(ActivationFunctionType.Linear, 
+                                                            ActivationFunctionType.Linear,
+                                                            ActivationFunctionType.Linear)
+                                              .Build()
+                                              .ToNetwork();
+
+            Assert.AreEqual(3, result.LayersCount);
+        }
+
+        [TestMethod]
+        public void AddLayer_InputIsSizesAndFunctions_ResultsLayersCountIs2()
+        {
+            var result = NeuralNetwork.Builder.AddNeurons(1, 2, 3, 4, 5)
+                                              .AddFunctions(ActivationFunctionType.Linear, 
+                                                            ActivationFunctionType.Linear)
+                                              .Build()
+                                              .ToNetwork();
+
+            Assert.AreEqual(2, result.LayersCount);
+        }
+
+        [TestMethod]
+        public void SetSerializer_InputIsNone_ReturnIsNull()
+        {
+            var result = NeuralNetwork.Builder.SetSerializer(SerializerType.None)
+                                              .Build()
+                                              .ToNetwork();
+
+            Assert.IsFalse(result.Serializer is ConfigSerializer);
+        }
+
+        [TestMethod]
+        public void SetSerializer_InputIsConfigSerializer_ReturnIsConfigSerializer()
+        {
+            var result = NeuralNetwork.Builder.SetSerializer(SerializerType.ConfigSerializer)
+                                              .Build()
+                                              .ToNetwork();
+
+            Assert.IsTrue(result.Serializer is ConfigSerializer);
+        }
+
+        [TestMethod]
+        public void IsBuildCorrect()
+        {
+            var result = NeuralNetwork.Builder.AddLayer(3, ActivationFunctionType.Sigmoid)
+                                              .SetInputSize(2)
+                                              .AddLayer(1, ActivationFunctionType.Sigmoid)
+                                              .Build()
+                                              .ToNetwork();
+
+            Assert.IsTrue(result.LayersCount == 2);
+            Assert.IsTrue(result.InputSize == 2);
+            Assert.IsTrue(result.OutputSize == 1);
+
+            Assert.IsTrue(result.Layers[0].NeuronsCount == 3);
+            Assert.IsTrue(result.Layers[0].ActivationFunction == ActivationFunctionType.Sigmoid);
+
+            Assert.IsTrue(result.Layers[1].NeuronsCount == 1);
+            Assert.IsTrue(result.Layers[1].ActivationFunction == ActivationFunctionType.Sigmoid);
+        }
+
+        [TestMethod]
+        public void BuildFromConfigFile()
+        {
+            var result = NeuralNetwork.Builder.BuildFromConfig(@"./../../../NNet/Network/test.ncfg")
+                                              .ToNetwork();
+
+            Assert.AreEqual(2, result.InputSize);
+            Assert.AreEqual(1, result.LayersCount);
+
+            double[] TEST_BIAS = { 1 };
+            double[,] TEST_WEIGHTS = { { 1 }, { 1 } };
+
+            Assert.AreEqual(2, result.Layers[0].InputSize);
+            Assert.AreEqual(1, result.Layers[0].NeuronsCount);
+            CollectionAssert.AreEqual(TEST_BIAS, result.Layers[0].Bias);
+            CollectionAssert.AreEqual(TEST_WEIGHTS, result.Layers[0].Weights);
+            Assert.AreEqual(ActivationFunctionType.Linear, result.Layers[0].ActivationFunction);
+        }
+    }
+}
---
+++ src/NNet.Test/Network/NetworkTests.cs
@@ -0,0 +1,79 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NNet.Functions;
+
+namespace NNet.Test.Network
+{
+    [TestClass]
+    public class NetworkTests
+    {
+        [TestMethod]
+        public void IsConvertCorrect_InputIsBuilderResult_ResultIsConvertedNetwork()
+        {
+            var test = NeuralNetwork.Builder.SetInputSize(2)
+                                            .AddNeurons(1, 2)
+                                            .AddFunctions(ActivationFunctionType.Linear, ActivationFunctionType.LReLU)
+                                            .Build();
+
+            NeuralNetwork result = test;
+
+            Assert.AreEqual(2, result.LayersCount);
+            Assert.AreEqual(2, result.InputSize);
+            Assert.AreEqual(2, result.OutputSize);
+
+            Assert.IsTrue(result.Layers[0].InputSize == 2);
+            Assert.IsTrue(result.Layers[0].NeuronsCount == 1);
+            Assert.IsTrue(result.Layers[0].ActivationFunction == ActivationFunctionType.Linear);
+
+            Assert.IsTrue(result.Layers[1].InputSize == 1);
+            Assert.IsTrue(result.Layers[1].NeuronsCount == 2);
+            Assert.IsTrue(result.Layers[1].ActivationFunction == ActivationFunctionType.LReLU);
+        }
+
+        [TestMethod]
+        public void IsCalculateCorrect_InputIs1And1_ResultIs2()
+        {
+            NeuralNetwork test = NeuralNetwork.Builder.SetInputSize(2)
+                                                      .AddLayer(1, ActivationFunctionType.Linear)
+                                                      .Build();
+
+            double[,] TEST_WEIGHTS = { { 1 }, { 1 } };
+            double[] TEST_INPUT = { 1, 1 };
+            double[] TEST_BIAS = { 0 };
+            double CORRECT_OUTPUT = 2;
+
+            test.Layers[0].Weights = TEST_WEIGHTS;
+            test.Layers[0].Bias = TEST_BIAS;
+
+            var result = test.GetResult(TEST_INPUT);
+
+            Assert.AreEqual(CORRECT_OUTPUT, result[0]);
+        }
+
+        [TestMethod]
+        public void IsLearnCorrect_InputErrorIs0And2_ResultIsCorrectWeightsAndBias()
+        {
+            NeuralNetwork test = NeuralNetwork.Builder.SetInputSize(2)
+                                                      .AddLayer(2, ActivationFunctionType.Linear)
+                                                      .Build();
+
+            double[,] TEST_WEIGHTS = { { 1, 1 }, { 1, 1 } };
+            double[] TEST_INPUT = { 1, -1 };
+            double[] TEST_BIAS = { 0, 0 };
+            double[] TEST_ERROR = { 0, 2 };
+            double TEST_RATE = 0.5;
+
+            double[,] CORRECT_WEIGHTS = { { 1, 2 }, { 1, 0 } };
+            double[] CORRECT_BIAS = { 0, 1 };
+
+            test.Layers[0].Weights = TEST_WEIGHTS;
+            test.Layers[0].Bias = TEST_BIAS;
+
+            test.GetResult(TEST_INPUT);
+            test.Learn(TEST_ERROR, TEST_RATE);
+
+            CollectionAssert.AreEqual(CORRECT_WEIGHTS, test.Layers[0].Weights);
+            CollectionAssert.AreEqual(CORRECT_BIAS, test.Layers[0].Bias);
+        }
+    }
+}
